6e5f9e3357026569a5b47402b5a1b97467c585e7
codeynamics <120318387+codeynamics@users.noreply.github.com>
2023-02-10 13:59:57 +0530

    Update ShipmentClass.java

--- functions/sarahForm/ShipmentClass.java
+++ functions/sarahForm/ShipmentClass.java
@@ -1,26 +1,39 @@
 import java.util.logging.Logger;
 import java.util.logging.Level;
-
+import java.util.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import com.zc.component.object.ZCObject;
+import com.zc.component.object.ZCRowObject;
+import com.zc.component.object.ZCTable;
+import com.zc.component.object.ZCRowObject; 
+import com.zc.component.zcql.ZCQL;
 import com.catalyst.advanced.CatalystAdvancedIOHandler;
 
-public class ShipmentClass implements CatalystAdvancedIOHandler {
-	private static final Logger LOGGER = Logger.getLogger(ShipmentClass.class.getName());
-	
-	@Override
+public class Analysis implements CatalystAdvancedIOHandler {
+    private static final Logger LOGGER = Logger.getLogger(Analysis.class.getName());
+    static String GET = "GET";
+    static String POST = "POST";
+    @Override
     public void runner(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		try {
-			String name = (String) request.getParameter("name");
-			LOGGER.log(Level.INFO, "Hello "+name);
-			response.setStatus(200);
-		}
-		catch(Exception e) {
-			LOGGER.log(Level.SEVERE,"Exception in ShipmentClass",e);
-			response.setStatus(500);
-		}
-        response.getWriter().write("Hello From ShipmentClass.java");
-	}
-	
+
+        
+        try {
+            String url = request.getRequestURI();
+            String method = request.getMethod();
+           String query = "SELECT * FROM Lead";
+           ArrayList<ZCRowObject> rowList = ZCQL.getInstance().executeQuery(query);
+            LOGGER.log(Level.INFO,"rowList: "+ rowList.get(0).get("bakery", "Cust_Id"));
+            response.setStatus(200);
+            for(int i =0;i<rowList.size();i++)
+                response.getWriter().write("rowList: "+ rowList.get(i).get("bakery", "FirstName"));
+            //String email = rowList.get(0).get("Lead", "Email");
+           
+        }
+        catch(Exception e) {
+            LOGGER.log(Level.SEVERE,"Exception in Analysis",e);
+            response.setStatus(500);
+        }
+    }
+    
 }
