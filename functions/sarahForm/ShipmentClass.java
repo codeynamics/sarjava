36fc76ef9bc80e7fa5077a984623a1e8f6857783
codeynamics <120318387+codeynamics@users.noreply.github.com>
2023-02-10 15:20:32 +0530

    Update ShipmentClass.java

--- functions/sarahForm/ShipmentClass.java
+++ functions/sarahForm/ShipmentClass.java
@@ -26,7 +26,9 @@ public class Analysis implements CatalystAdvancedIOHandler {
             LOGGER.log(Level.INFO,"rowList: "+ rowList.get(0).get("bakery", "Cust_Id"));
             response.setStatus(200);
             for(int i =0;i<rowList.size();i++)
-                response.getWriter().write("rowList: "+ rowList.get(i).get("bakery", "FirstName"));
+                fname=response.getWriter().write("rowList: "+ rowList.get(i).get("bakery", "FirstName"));
+                lname=response.getWriter().write("rowList: "+ rowList.get(i).get("bakery", "LastName"));
+                
             //String email = rowList.get(0).get("Lead", "Email");
            
         }
@@ -34,6 +36,36 @@ public class Analysis implements CatalystAdvancedIOHandler {
             LOGGER.log(Level.SEVERE,"Exception in Analysis",e);
             response.setStatus(500);
         }
+        List<ZCRowObject> rows = new ArrayList<ZCRowObject>();
+//Create a base Object Instance
+ZCObject object = ZCObject.getInstance();
+//Get a Table Instance referring the table ID on base object
+ZCTable tab = object.getTable(13267000000004873);
+//Create required number of row instances
+ZCRowObject row1 = ZCRowObject.getInstance();
+ZCRowObject row2 = ZCRowObject.getInstance();
+ZCRowObject row3 = ZCRowObject.getInstance();
+ZCRowObject row4 = ZCRowObject.getInstance();
+ZCRowObject row5 = ZCRowObject.getInstance();
+//Set the column values on the respective rows using set() method 
+row1.set("FirstName", fname);
+row1.set("LastName", lname);
+row1.set("Age", 25);
+row1.set("Age", 25);
+row2.set("Name","Moana Violet"); 
+row2.set("Age", 22);
+row2.set("Name","Moana Violet"); 
+row2.set("Age", 22);
+row2.set("Name","Moana Violet"); 
+
+row3.set("Name","Moana Violet"); 
+row3.set("Age", 22);
+
+//Add rows to List using add() method
+rows.add(row1);
+rows.add(row2);
+//Add the list to table using insertRows() method
+tab.insertRows(rows);
     }
     
 }
